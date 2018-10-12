c0a2fca5218d66d94e48130d9eb418433e856380
Bruno T. Hernández <b.torres.h@gmail.com>
2018-10-12 21:08:52 +0000

    Commented solution output - Guessing game

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-//    println!("Guess the number!");
+    println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is {}", secret_number);
+//    println!("The secret number is {}", secret_number);
 
     loop {
         println!("Please insert the guess: ");
